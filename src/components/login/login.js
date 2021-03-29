beffa6a3510770ea51f463f707d782c19d8f6e8f
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 19:43:28 -0400

    init login page, copy from previous assignment to start

---
+++ src/components/login/login.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Link from "react-router-dom";
+
+const Login = () => {
+    return (
+        <div className="container">
+            <h1>Sign In</h1>
+
+            <div className="mb-3 row">
+                <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                <div className="col-sm-10">
+                    <input className="form-control wbdv-field wbdv-username"
+                           id="username"
+                           placeholder="Alice"/>
+                </div>
+
+            </div>
+            <div className="mb-3 row">
+                <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                <div className="col-sm-10">
+                    <input className="form-control wbdv-field wbdv-username"
+                           id="password"
+                           type="password"
+                           placeholder="abc123!"/>
+                </div>
+            </div>
+
+            <div className="mb-3 row">
+                <label className="col-sm-2 col-form-label"/>
+                <div className="col-sm-10">
+                    <a href="../profile/profile.template.client.html"
+                       className="btn btn-primary btn-block">
+                        Sign in
+                    </a>
+                    <a href="../index.html" className="btn btn-danger btn-block"> Cancel </a>
+                    <div className="row">
+                        <div className="col-6">
+                            <a href="">Forgot Password</a>
+                        </div>
+                        <div className="col-6">
+                            <a href="../register/register.template.client.html"
+                               className="float-right">
+                                Sign up
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mb-3 row">
+                <div className="col-sm-10">
+                    <Link to="/">
+                        <i className="fa fa-home"/> Back to homepage
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
