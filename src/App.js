b2721b47139510ea7e13d90275b905d3accecb30
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 19:43:45 -0400

    add router for login page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import Search from "./components/search";
 import Details from "./components/details";
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/login/login";
 
 function App() {
   return (
       <div className="container-fluid">
-        <BrowserRouter>
-          <Route path="/" exact={true}>
-            <Home/>
-          </Route>
-          <Route path={["/search", "/search/:name"]}
-                 exact={true}>
-            <Search/>
-          </Route>
-          <Route path="/details/:id" exact={true}>
-            <Details/>
-          </Route>
+          <BrowserRouter>
+              <Route path="/" exact={true}>
+                  <Home/>
+              </Route>
+              <Route path={["/search", "/search/:name"]}
+                     exact={true}>
+                  <Search/>
+              </Route>
+              <Route path="/details/:id" exact={true}>
+                  <Details/>
+              </Route>
+              <Route path="/login" exact={true}>
+                  <Login/>
+              </Route>
         </BrowserRouter>
       </div>
   );
