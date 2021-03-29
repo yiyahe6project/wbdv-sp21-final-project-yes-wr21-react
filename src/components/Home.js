4029efd6b51d5f6316add0c72109c27c6da807b5
Jiahao Cai <cai.jiah@husky.neu.edu>
2021-03-29 19:43:57 -0400

    add Link to login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ const Home = () => {
     return(
         <div>
             <h2>Home</h2>
+            <Link to="/login">
+                Login
+            </Link>
             <Link to="/search">
                 Search
             </Link>
